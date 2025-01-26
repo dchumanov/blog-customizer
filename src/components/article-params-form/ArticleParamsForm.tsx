8efda99534acb72faa7e7a317d08d834ff686a46
Dmitry Chumanov <redenergy-52@yandex.ru>
2025-01-26 19:27:34 +0300

    third re

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -5,10 +5,19 @@ import clsx from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
 import { useRef, useState } from 'react';
 import { Select } from 'src/ui/select';
-import { ArticleStateType, fontSizeOptions } from 'src/constants/articleProps';
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
 import { Text } from 'src/ui/text';
 import { Separator } from 'src/ui/separator';
 import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
+import { RadioGroup } from 'src/ui/radio-group';
 
 type ArticleParamsFormProps = {
 	currentArticle: ArticleStateType;
@@ -30,6 +39,17 @@ export const ArticleParamsForm = ({
 		});
 	};
 
+	const handleFormReset = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setCurrentArticle({
+			...defaultArticleState,
+		});
+		setSelectArticleData({
+			...selectArticleData,
+			...defaultArticleState,
+		});
+	};
+
 	useOutsideClickClose({
 		isOpen,
 		rootRef,
@@ -43,11 +63,29 @@ export const ArticleParamsForm = ({
 			<ArrowButton isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
 			<aside
 				className={clsx(styles.container, isOpen && styles.container_open)}>
-				<form className={styles.form} onSubmit={handleFormSubmit}>
-					<Text size={38}>Размер шрифта</Text>
+				<form
+					className={styles.form}
+					onSubmit={handleFormSubmit}
+					onReset={handleFormReset}>
+					<Text as={'h2'} size={31} uppercase={true} weight={800}>
+						Задайте параметры
+					</Text>
 					<Select
-						options={fontSizeOptions}
+						options={fontFamilyOptions}
+						selected={selectArticleData.fontFamilyOption}
+						title='Шрифт'
+						onChange={(data) =>
+							setSelectArticleData({
+								...selectArticleData,
+								fontFamilyOption: data,
+							})
+						}
+					/>
+					<RadioGroup
 						selected={selectArticleData.fontSizeOption}
+						name='fontsize_radio'
+						options={fontSizeOptions}
+						title='Размер шрифта'
 						onChange={(data) =>
 							setSelectArticleData({
 								...selectArticleData,
@@ -55,7 +93,40 @@ export const ArticleParamsForm = ({
 							})
 						}
 					/>
+					<Select
+						options={fontColors}
+						selected={selectArticleData.fontColor}
+						title='Цвет шрифта'
+						onChange={(data) =>
+							setSelectArticleData({
+								...selectArticleData,
+								fontColor: data,
+							})
+						}
+					/>
 					<Separator />
+					<Select
+						options={backgroundColors}
+						selected={selectArticleData.backgroundColor}
+						title='Цвет фона'
+						onChange={(data) =>
+							setSelectArticleData({
+								...selectArticleData,
+								backgroundColor: data,
+							})
+						}
+					/>
+					<Select
+						options={contentWidthArr}
+						selected={selectArticleData.contentWidth}
+						title='Ширина контента'
+						onChange={(data) =>
+							setSelectArticleData({
+								...selectArticleData,
+								contentWidth: data,
+							})
+						}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
